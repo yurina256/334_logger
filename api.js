c8f661d356b1afaa083f28a18acf4091e62d0137
yurina256 <hoge.huga@yurina256>
2021-06-07 07:43:14 +0900

    進捗

--- api.js
+++ api.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-app.use(express.static('home'));
+
+app.use(express.static('Horloge'));
+app.use(express.static("about"));
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/api',(req,res) => {
     var json = {hoge:"huga"};
     res.json(json);
 });
+app.get("/about",(req,res) => {
+    res.sendFile(__dirname + "/about/index.html");
+})
 app.listen(3000, () => console.log('Listening on port 3000'));
